9b87d1885d7c18253f39390d2f5738e36e9f7fea
Robert P <163090599+RobertParesi@users.noreply.github.com>
2024-07-09 13:07:26 -0700

    Update Setlist Copy Button-1.0.user.js

--- Setlist Copy Button-1.0.user.js
+++ Setlist Copy Button-1.0.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Setlist Copy Button
+// @name         Setlist FM Clipboard
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Copy Setlist from Setlist
-// @author       Robert
+// @version      2024-07-09
+// @description  Put Setlist Information to Clipboard
+// @author       Jerry Garcia
 // @match        https://www.setlist.fm/setlist/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=setlist.fm
 // @grant        none
@@ -52,37 +52,67 @@ function GetDetail(WithSets) {
     section = "";
 
     let lis=document.querySelectorAll('li.setlistParts');
-    lis.forEach((li)=> {
+    if(lis == null) {
+        alert('Could not find setlistParts!');
+    } else {
+        lis.forEach((li)=> {
 
-        let classDesc = li.className;
-        if(classDesc.includes("section") == true || classDesc.includes("encore") == true ) {
-            section = li.innerText.replace(":","");
-            if(WithSets == 1) {
-                if(count > 0) {
-                    FullString = FullString + CRLF;
+            let classDesc = li.className;
+            if(classDesc.includes("section") == true || classDesc.includes("encore") == true ) {
+                section = li.innerText.replace(":","");
+                if(WithSets == 1) {
+                    if(count > 0) {
+                        FullString = FullString + CRLF;
+                    }
+                    FullString = FullString + section + ":" + CRLF;
+                    count+=1;
                 }
-                FullString = FullString + section + ":" + CRLF;
-                count+=1;
             }
-        }
-        if(classDesc.includes("song") == true ) {
-            let nindex = li.innerText.indexOf("\n");
-            let SongName = li.innerText.slice(0,nindex) ;
-            if(WithSets == 1) {
-                FullString = FullString + " • " + SongName + CRLF;
-                count+=1;
-            } else {
-                FullString = FullString + TheDate + ',' + TheBand + ',' + TheVenue + ',"' + SongName + '",' + section + CRLF;
-                count+=1;
+            if(classDesc.includes("song") == true ) {
+                let nindex = li.innerText.indexOf("\n");
+                let SongName = li.innerText.slice(0,nindex) ;
+                let InfoPart = li.innerText.slice(nindex);
+                InfoPart = InfoPart.replaceAll("Play Video","");
+                InfoPart = ImportantParts(InfoPart);
+                if(WithSets == 1) {
+                    FullString = FullString + " • " + SongName.trim() + " " + InfoPart.trim() + CRLF;
+                    count+=1;
+                } else {
+                    FullString = FullString + TheDate + ',' + TheBand + ',' + TheVenue + ',"' + SongName + '",' + section + CRLF;
+                    count+=1;
+                }
             }
-        }
-    });
+        });
+    }
 }
+
+function ImportantParts(InPart) {
+
+    let WorkString = "";
+    let FinalString = "";
+    let active = false;
+
+    for (let i = 0; i < InPart.length; i++) {
+        if(InPart[i] = '(') { active = true };
+        if(active == true) {
+            WorkString = WorkString + InPart[i];
+            if(InPart[i] == ')') {
+                let CheckString = WorkString.toLowerCase();
+                if (CheckString.includes("cover") == false) {
+                    FinalString = FinalString + WorkString + " ";
+                }
+                WorkString = "";
+                active = false;
+            }
+        }
+    }
+    return FinalString;
+}
+
 
 function CustomSetClipboard(WithSets,inDesc) {
 
     FullString = "";
-
     GetHeader();
     GetDetail(WithSets);
     navigator.clipboard.writeText(FullString);
@@ -100,7 +130,6 @@ function CustomSetClipboard(WithSets,inDesc) {
                 const newButton = document.createElement('button');
                 newButton.textContent = 'Song Copy';
                 newButton.style = buttonStyle;
-
                 c_button.after(newButton);
                 newButton.addEventListener('click', () => {
                     CustomSetClipboard(0,'Songs');
